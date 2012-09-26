0ef230681ae02c5a0af3c1eba71b8b0a77b2f89c
Bjoern Klippstein <klippstein@klippstein.com>
2012-09-26 10:44:49 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klippdemo::Application.config.secret_token = 'b5a51335438136805fbc97f2e2838186b69a7663dc0f1cea5dba21046bc2f3194b7055235f1e68fdf55d8ec2b4588eeee389dacbecb2ea576b74ec9e042a43cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Klippdemo::Application.config.session_store :cookie_store, key: '_klippdemo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Klippdemo::Application.config.session_store :active_record_store
